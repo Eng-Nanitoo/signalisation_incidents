dfba9c8f36257ad625fa0f7bff3c692109054af2
Eng-Nanitoo <i20378.etu@iscae.mr>
2025-04-26 14:59:05 +0000

    Create Incidents,Categories Models

--- signalisation_incidents/incidents/models.py
+++ signalisation_incidents/incidents/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Categorie(models.Model):
     nom = models.CharField(max_length = 100)
+
+class Etat(models.TextChoices):
+    ATTENTE = 'AT', _('En Attente')
+    TRAITE = 'TR', _('Traite')
 
 class Incident(models.Model):
     photo = models.ImageField()
@@ -9,6 +14,6 @@ class Incident(models.Model):
     latitude = models.PositiveIntegerField()
     description_texte = models.TextField()
     description_vocale = models.FileField()
-    etat = models.TextChoices(value= {"en attente", "traite"})
+    etat = models.CharField(choices = Etat.choices,default = Etat.ATTENTE)
     date_creation = models.DateTimeField(auto_now_add=True)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
